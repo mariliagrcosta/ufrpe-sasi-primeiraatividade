f36b01ae4048272a64fba73c61bf8e005ed489c6
marilia.costa <51496973+mariliagrcosta@users.noreply.github.com>
2023-12-03 02:47:12 -0300

    .java compilado

--- applet_alternative.js
+++ applet_alternative.js
@@ -1,4 +1,5 @@
 <object type="application/x-java-applet" height="600" width="200">
-<param name="code" value="messageApplet.class" />
+<param name="codebase" value="java" />
+<param name="archive" value="messageApplet.class" />
 O APPLET NÃO PÔDE SER EXEBIDO!
 </object>
