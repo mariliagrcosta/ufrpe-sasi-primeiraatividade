b53d3eb749cda8fd97e913cdc0360303cde9195b
marilia.costa <51496973+mariliagrcosta@users.noreply.github.com>
2023-12-06 04:26:30 -0300

    Ajuste do tamanho do applet

--- applet_alternative.js
+++ applet_alternative.js
@@ -1,4 +1,4 @@
-<object type="application/x-java-applet" height="600" width="200">
+<object type="application/x-java-applet" height="200" width="200">
 <param name="codebase" value="java" />
 <param name="archive" value="messageApplet.class" />
 </object>
