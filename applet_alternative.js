7ed92fe541e59b4a927a0cfac23e47e867e84a66
marilia.costa <51496973+mariliagrcosta@users.noreply.github.com>
2023-12-06 04:23:36 -0300

    Retirada do informe 2

--- applet_alternative.js
+++ applet_alternative.js
@@ -1,5 +1,4 @@
 <object type="application/x-java-applet" height="600" width="200">
 <param name="codebase" value="java" />
 <param name="archive" value="messageApplet.class" />
-O APPLET NÃO PÔDE SER EXEBIDO!
 </object>
