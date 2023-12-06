3a559debd5bb1389a207a5e9986b4f9dbcaa6aee
marilia.costa <51496973+mariliagrcosta@users.noreply.github.com>
2023-12-06 04:45:59 -0300

    Mudança do nome do método

--- java/Main.java
+++ java/Main.java
@@ -2,7 +2,7 @@ import java.applet.Applet;
 import java.awt.Graphics;
 
 public class Main extends Applet {
-    public void draw(Graphics g) {
+    public void paint(Graphics g) {
 
         g.drawString("VOCÊ FOI HACKEADO!", 1, 1);
         
