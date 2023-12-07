b14515a46e1211fe6956007ed2975e4a3ee44a56
marilia.costa <51496973+mariliagrcosta@users.noreply.github.com>
2023-12-07 02:16:41 -0300

    Definição da cor do texto

--- java/Main.java
+++ java/Main.java
@@ -1,10 +1,12 @@
 import java.applet.Applet;
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class Main extends Applet {
     public void paint(Graphics g) {
 
-        g.drawString("VOCÊ FOI HACKEADO!", 1, 1);
+        g.setColor(Color.MAGENTA);
+        g.drawString("VOCÊ FOI HACKEADO!", 50, 50);
         
     }
     
